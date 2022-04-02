e41d365b020c8e28a6c16424f883f27dfe623cb5
AlvaroRGZ <alu0101362953@ull.edu.es>
2022-04-02 13:31:13 +0000

    Inicio declaracion de clases

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,54 @@
-/*
-* @param firstnumber aaaa
-* @param secondnumber aaaaaa
-* @return bbbbbb
-* ```typescript
-*     add(1,7) = 8
-*```
-*/
-
-export function add(firstnumber: number, secondnumber: number): number {
-  return firstnumber + secondnumber;
+export class Grupo {
+  constructor(
+    private nombre_: string,
+    private componentes_: Artista[], /* o Artista*/
+    private añoCreacion_: number,
+    private generos_: Genero[],
+    private albumes_: Album[],
+    private oyentesMensuales_: number) {}
 }
-console.log(`add(1,7): ${add(1, 7)}`);
 
-
-export function resta(firstnumber: number, secondnumber: number): number {
-  return firstnumber - secondnumber;
+export class Artista {
+  constructor(
+    private nombre_: string,
+    private grupos_: Grupo[], /* o Artista*/
+    private generos_: Genero[],
+    private albumes_: Album[],
+    private canciones_: Cancion[],
+    private oyentesMensuales_: number /* suma de los oyentes en sus grupos mas las de sus canciones propias */) {}
 }
-console.log(`resta(1,7): ${resta(1, 7)}`);
 
-
-export function multiplicacion(firstnumber: number, secondnumber: number): number {
-  return firstnumber * secondnumber;
+export class Genero {
+  constructor(
+    private nombre_: string,
+    private gruposOArtistas_: (Grupo | Artista)[], /* hay que hacer una interfaz generica para no usar union de tipos*/
+    private albumes_: Album[],
+    private canciones_: Cancion[]) {}
 }
-console.log(`multiplicacion(1,7): ${multiplicacion(1, 7)}`);
 
-
-export function division(firstnumber: number, secondnumber: number): number {
-  return firstnumber / secondnumber;
+export class Cancion {
+  constructor(
+    private nombre_: string,
+    private autor_: string, /* o Artista*/
+    private duracion_: string,
+    private single_: boolean,
+    private reproducciones_: number) {}
 }
-console.log(`division(1,7): ${division(1, 7)}`);
+
+export class Album {
+  constructor(
+    private nombre_: string,
+    private autor_: string, /* o Artista*/
+    private añoPublicacion_: number,
+    private generos_: Genero[],
+    private reproducciones_: number) {}
+}
+
+export class PlayList {
+  constructor(
+    private nombre_: string,
+    private autor_: string, /* o Artista*/
+    private canciones_: Cancion[],
+    private generos_: Genero[],
+    private duracion_: string) {}
+}
