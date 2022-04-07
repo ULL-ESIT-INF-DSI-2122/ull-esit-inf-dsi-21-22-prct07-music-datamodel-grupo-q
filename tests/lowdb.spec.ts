49cf9ff0265f6c900494344e95c3aa49d3215186
Daniel Hernández Fajardo <alu0101320489@ull.edu.es>
2022-04-08 00:57:37 +0100

    pruebas para lowdb

---
+++ tests/lowdb.spec.ts
@@ -0,0 +1,12 @@
+import 'mocha';
+import {expect} from 'chai';
+import {JsonCancionCollection} from '../src/estructura/cancion';
+
+let col = new JsonCancionCollection([]);
+describe('prueba lowdb', () => {
+    it('prueba', () => {
+        // col.addCancion('Buleria', 'Bisbal', [], '2:10', true, 50000);
+        // col.deleteCancion('Buleria');
+        expect(col.getCancion(0).getNombre()).to.eql('Alone');
+    });
+  });
