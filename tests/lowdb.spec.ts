46f348eb237aff17f7f641da3ced6250bc32904a
Javier Díaz <alu0101128894@ull.edu.es>
2022-04-11 00:08:27 +0000

    coveralls github actions

--- tests/lowdb.spec.ts
+++ tests/lowdb.spec.ts
@@ -5,7 +5,7 @@ import {JsonCancionCollection} from '../src/estructura/cancion';
 let col = new JsonCancionCollection([]);
 describe('prueba lowdb', () => {
     it('prueba', () => {
-        col.addCancion('Buleria', 'Bisbal', [], '2:10', true, 50000);
+        col.addCancion('Buleria', 'Bisbal', ["Pop"], '2:10', true, 50000);
         // col.deleteCancion('Buleria');
         expect(col.getCancion(0).getNombre()).to.eql('Alone');
     });
